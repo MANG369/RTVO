bd6d61885cda1eb67e5d1317fa79ab83c2f0d678
MANG369 <nievesgarciamarcoantonio@gmail.com>
2026-08-27 04:12:12 -1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,298 @@
+const playlistCategorias = {
+    AMOR: [
+        { title: "Amor 1", id: "bjw_PUWq9zE" },
+        { title: "Amor 2", id: "Y_0gUaCb1C8" },
+        { title: "Amor 3", id: "cMhT1SqxqHY" },
+        { title: "Amor 4", id: "4dyjL34qDis" },
+        { title: "Amor 5", id: "NgVmz3gfHd0" },
+        { title: "Amor 6", id: "5UlLN3bXdqk" },
+        { title: "Amor 7", id: "_OF-AaUlxCg" },
+        { title: "Amor 8", id: "wC_sOpQjvWE" },
+        { title: "Amor 9", id: "rEH3QdOogtY" },
+        { title: "Amor 10", id: "H1pnb-P68I0" },
+        { title: "Amor 11", id: "XEvKn-QgAY0" },
+        { title: "Amor 12", id: "HfzbN5ky5Co" },
+        { title: "Amor 13", id: "mJGRQ5gZmmc" },
+        { title: "Amor 14", id: "lmDfmilZ4Xo" },
+        { title: "Amor 15", id: "15iEJ0qY_70" },
+        { title: "Amor 16", id: "_6HpI5i84w8" }
+    ],
+    EL_PATRON: [
+        { title: "El Patrón 1", id: "LruRZKY3KFY" },
+        { title: "El Patrón 2", id: "U3fMHWnUWLI" },
+        { title: "El Patrón 3", id: "6Lielvgiu3E" },
+        { title: "El Patrón 4", id: "Twazrsyj8DA" }
+    ],
+    ARABE: [
+        { title: "Árabe 1", id: "Qz8agovq_kc" },
+        { title: "Árabe 2", id: "bVjbHxJ_7U0" },
+        { title: "Árabe 3", id: "btYNNu8G5s4" },
+        { title: "Árabe 4", id: "GwTj6i-ce9A" },
+        { title: "Árabe 5", id: "wF6X598iTxs" },
+        { title: "Árabe 6", id: "xjR_hZBey-Y" },
+        { title: "Árabe 7", id: "HuXQt07KjYc" },
+        { title: "Árabe 8", id: "E3fLw84RrZI" },
+        { title: "Árabe 9", id: "y6w8wcE2QK0" },
+        { title: "Árabe 10", id: "7thhTKR0B_o" },
+        { title: "Árabe 11", id: "4R5zsoxjARk" },
+        { title: "Árabe 12", id: "xYsUSIuRhr0" },
+        { title: "Árabe 13", id: "bU5f58LmLcA" },
+        { title: "Árabe 14", id: "EFj2S5KEmfk" },
+        { title: "Árabe 15", id: "z1YdK8SmL4w" },
+        { title: "Árabe 16", id: "LOh-9SuhJS4" },
+        { title: "Árabe 17", id: "vhY1LsKWpVQ" },
+        { title: "Árabe 18", id: "8KsFOMXdqQs" },
+        { title: "Árabe 19", id: "ib7RvtJGyuk" },
+        { title: "Árabe 20", id: "6rnOKs2_El0" },
+        { title: "Árabe 21", id: "iuvnKEvHMUk" },
+        { title: "Árabe 22", id: "c9CXSbWIpMs" },
+        { title: "Árabe 23", id: "vFcTqzeHiBk" },
+        { title: "Árabe 24", id: "ddq7TDJ2_VI" },
+        { title: "Árabe 25", id: "PWhTtI0FBo4" },
+        { title: "Árabe 26", id: "4gFtq4TQJFg" }
+    ],
+    SALSA: [
+        { title: "Salsa 1", id: "TwGU4D-f3KM" },
+        { title: "Salsa 2", id: "8npywMdv0Sw" },
+        { title: "Salsa 3", id: "D_0FKJqxldE" },
+        { title: "Salsa 4", id: "1oLnDNwXo-k" },
+        { title: "Salsa 5", id: "Lg13W7_H3d8" },
+        { title: "Salsa 6", id: "ZkmZtypSAAI" },
+        { title: "Salsa 7", id: "HWSNe9XTshM" },
+        { title: "Salsa 8", id: "sJqDmVekMWU" },
+        { title: "Salsa 9", id: "vdzKp-7XH6g" },
+        { title: "Salsa 10", id: "fErGESfO33M" },
+        { title: "Salsa 11", id: "ZGhstUQQous" },
+        { title: "Salsa 12", id: "41DPpQy8_QE" },
+        { title: "Salsa 13", id: "H5gxKL5pmI4" },
+        { title: "Salsa 14", id: "EXueOQed74E" },
+        { title: "Salsa 15", id: "PFAjmkab42o" },
+        { title: "Salsa 16", id: "UlOfqgBTtrU" },
+        { title: "Salsa 17", id: "NshE75Ok898" },
+        { title: "Salsa 18", id: "SUgQHe902yQ" },
+        { title: "Salsa 19", id: "czJ7N1KiSBk" },
+        { title: "Salsa 20", id: "TnldDo_T6i8" }
+    ],
+    MEDICINA: [
+        { title: "Medicina 1", id: "LfrBaI3Gf_g" },
+        { title: "Medicina 2", id: "rHuUa0radLw" },
+        { title: "Medicina 3", id: "x2hg9m1oJJc" },
+        { title: "Medicina 4", id: "0iQls6QVgn0" },
+        { title: "Medicina 5", id: "zd6qAJNV7us" },
+        { title: "Medicina 6", id: "jxyxos55MaE" },
+        { title: "Medicina 7", id: "Y2Y7n4v2kAA" },
+        { title: "Medicina 8", id: "JRE8M7__Szc" },
+        { title: "Medicina 9", id: "C_MdvYU5QtU" },
+        { title: "Medicina 10", id: "CFjkooRVUeM" },
+        { title: "Medicina 11", id: "1oQ-k7v2dOU" },
+        { title: "Medicina 12", id: "1Wt0Q_6gZcc" },
+        { title: "Medicina 13", id: "EtSz1qB5bPw" },
+        { title: "Medicina 14", id: "4ndQZgX6r7Q" },
+        { title: "Medicina 15", id: "MZeuf-4Ed70" },
+        { title: "Medicina 16", id: "7cRD4lLXkOA" },
+        { title: "Medicina 17", id: "BOGY5b5xDM8" },
+        { title: "Medicina 18", id: "6dlxfgn9h8s" },
+        { title: "Medicina 19", id: "clOM9DQB9ks" },
+        { title: "Medicina 20", id: "_ueaM7Ihb_I" },
+        { title: "Medicina 21", id: "_yWsnL8Xr_w" },
+        { title: "Medicina 22", id: "PZunNIS0W0M" },
+        { title: "Medicina 23", id: "TOU9x56QlfM" },
+        { title: "Medicina 24", id: "gQ1IzAI_scw" },
+        { title: "Medicina 25", id: "osKcv5iyL1E" },
+        { title: "Medicina 26", id: "vhUsvLt2_M4" },
+        { title: "Medicina 27", id: "F5D2sms1MSs" },
+        { title: "Medicina 28", id: "tIZ0Gp_ChGw" },
+        { title: "Medicina 29", id: "ZcDm5Aa5254" },
+        { title: "Medicina 30", id: "WQ4v2LcY7pw" },
+        { title: "Medicina 31", id: "rvfY9tUHTYA" },
+        { title: "Medicina 32", id: "mOOuivbwijY" },
+        { title: "Medicina 33", id: "Y2w7rBl1EqE" },
+        { title: "Medicina 34", id: "eBmZQiwlDME" },
+        { title: "Medicina 35", id: "Sfe8sRDMTAI" },
+        { title: "Medicina 36", id: "igMqVfieIBI" },
+        { title: "Medicina 37", id: "QZhuI5fNW9k" },
+        { title: "Medicina 38", id: "cfoXYZfMfWQ" },
+        { title: "Medicina 39", id: "5jF_lisChhA" },
+        { title: "Medicina 40", id: "jiG8LDcsCHA" }
+    ],
+    LLANERAS: [
+        { title: "Llanera 1", id: "FImOYoyORE4" },
+        { title: "Llanera 2", id: "gXBWmWq9FsU" },
+        { title: "Llanera 3", id: "vTF3PlSvb78" },
+        { title: "Llanera 4", id: "1hddRcwKZH0" },
+        { title: "Llanera 5", id: "agw86S9pIKk" },
+        { title: "Llanera 6", id: "HecBEZGxovk" },
+        { title: "Llanera 7", id: "suIYXr8kAlM" },
+        { title: "Llanera 8", id: "C6GAfwao84s" },
+        { title: "Llanera 9", id: "D-Zd02r-p-Y" },
+        { title: "Llanera 10", id: "hNlu7-0-zhs" },
+        { title: "Llanera 11", id: "Qtg-YlQTO6s" },
+        { title: "Llanera 12", id: "33Ne9VeIvpM" },
+        { title: "Llanera 13", id: "kpy2d0s2krY" },
+        { title: "Llanera 14", id: "tWac2wietKg" },
+        { title: "Llanera 15", id: "TdEimh4QcT8" },
+        { title: "Llanera 16", id: "7CF1hPg7tEg" },
+        { title: "Llanera 17", id: "4Y3jV2St_f8" }
+    ],
+    GENERAL: [
+        { title: "General 1", id: "GZ3zL7kT6_c" },
+        { title: "General 2", id: "Jp7jM7yJKB4" },
+        { title: "General 3", id: "WRcCBI5rFfM" },
+        { title: "General 4", id: "4RveUP8fKA4" },
+        { title: "General 5", id: "2tkT5Divh0A" },
+        { title: "General 6", id: "z_qWXNa6WVQ" },
+        { title: "General 7", id: "6EO1DvZXI44" },
+        { title: "General 8", id: "YlUKcNNmywk" },
+        { title: "General 9", id: "7iNbnineUCI" },
+        { title: "General 10", id: "JnfyjwChuNU" },
+        { title: "General 11", id: "DnGdoEa1tPg" },
+        { title: "General 12", id: "iywaBOMvYLI" },
+        { title: "General 13", id: "9Ht5RZpzPqw" },
+        { title: "General 14", id: "CSvFpBOe8eY" },
+        { title: "General 15", id: "7gwO8-oqwFw" },
+        { title: "General 16", id: "LH-i8IvYIcg" },
+        { title: "General 17", id: "sT0g16_LQaQ" },
+        { title: "General 18", id: "z7hhDINyBP0" },
+        { title: "General 19", id: "K7l5ZeVVoCA" },
+        { title: "General 20", id: "eW2qlKa6oHw" },
+        { title: "General 21", id: "8SbUC-UaAxE" },
+        { title: "General 22", id: "kPBzTxZQG5Q" },
+        { title: "General 23", id: "QSAYlMRKVzE" },
+        { title: "General 24", id: "5IpYOF4Hi6Q" },
+        { title: "General 25", id: "pkcJEvMcnEg" },
+        { title: "General 26", id: "-1_ZvjxUXiw" },
+        { title: "General 27", id: "QtTR-_Klcq8" },
+        { title: "General 28", id: "ZpUYjpKg9KY" },
+        { title: "General 29", id: "zUzd9KyIDrM" },
+        { title: "General 30", id: "jRGrNDV2mKc" }
+    ]
+};
+
+let player;
+let isPlaying = false;
+let currentCategory = "AMOR";
+let currentSongIndex = 0;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('hidden-player', {
+        height: '0',
+        width: '0',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    renderPlaylist();
+    updateSongInfo();
+    // Precarga (cue) el primer video sin forzar reproducción automática bloqueable
+    const firstSong = playlistCategorias[currentCategory][0];
+    if (player && player.cueVideoById) {
+        player.cueVideoById(firstSong.id);
+    }
+}
+
+function onPlayerStateChange(event) {
+    // Sincroniza estado de reproducción real enviado por la API
+    if (event.data === YT.PlayerState.PLAYING) {
+        isPlaying = true;
+        updateUIState();
+    } else if (event.data === YT.PlayerState.PAUSED) {
+        isPlaying = false;
+        updateUIState();
+    } else if (event.data === YT.PlayerState.ENDED) {
+        nextSong();
+    }
+}
+
+function changeCategory() {
+    const select = document.getElementById("categorySelect");
+    currentCategory = select.value;
+    currentSongIndex = 0;
+    renderPlaylist();
+    loadSong(0);
+}
+
+function renderPlaylist() {
+    const playlistEl = document.getElementById("playlist");
+    playlistEl.innerHTML = "";
+    
+    playlistCategorias[currentCategory].forEach((song, index) => {
+        const li = document.createElement("li");
+        li.textContent = `${index + 1}. ${song.title}`;
+        if (index === currentSongIndex) li.classList.add("active");
+        li.onclick = () => loadSong(index);
+        playlistEl.appendChild(li);
+    });
+}
+
+function updateSongInfo() {
+    const song = playlistCategorias[currentCategory][currentSongIndex];
+    document.getElementById("songTitle").textContent = song.title;
+    document.getElementById("songArtist").textContent = `Categoría: ${currentCategory.replace('_', ' ')}`;
+    
+    const items = document.querySelectorAll("#playlist li");
+    items.forEach((item, i) => {
+        item.classList.toggle("active", i === currentSongIndex);
+    });
+}
+
+function loadSong(index) {
+    currentSongIndex = index;
+    const song = playlistCategorias[currentCategory][index];
+    
+    if (player && player.loadVideoById) {
+        player.loadVideoById(song.id);
+        isPlaying = true;
+        updateUIState();
+    }
+    updateSongInfo();
+}
+
+function togglePlay() {
+    if (!player) return;
+    
+    if (isPlaying) {
+        player.pauseVideo();
+    } else {
+        const song = playlistCategorias[currentCategory][currentSongIndex];
+        const videoData = player.getVideoData ? player.getVideoData() : null;
+        
+        if (!videoData || !videoData.video_id) {
+            player.loadVideoById(song.id);
+        } else {
+            player.playVideo();
+        }
+    }
+}
+
+function updateUIState() {
+    const playBtn = document.getElementById("playBtn");
+    const visualizer = document.getElementById("visualizer");
+    
+    if (isPlaying) {
+        playBtn.innerHTML = '<i class="fa-solid fa-pause"></i>';
+        visualizer.classList.add("playing");
+    } else {
+        playBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
+        visualizer.classList.remove("playing");
+    }
+}
+
+function prevSong() {
+    currentSongIndex--;
+    if (currentSongIndex < 0) {
+        currentSongIndex = playlistCategorias[currentCategory].length - 1;
+    }
+    loadSong(currentSongIndex);
+}
+
+function nextSong() {
+    currentSongIndex++;
+    if (currentSongIndex >= playlistCategorias[currentCategory].length) {
+        currentSongIndex = 0;
+    }
+    loadSong(currentSongIndex);
+}
